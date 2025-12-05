824a4297d34e88b881acbdc6152b8c216c21dcf7
Artem-sudo25 <akritski@ymail.com>
2025-12-05 16:18:11 +0100

    Fix CORS headers for HaloTrack

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: "Access-Control-Allow-Origin",
-            value: "https://www.propradlo.cz,https://propradlo.cz",
+            value: "*", // Allow all origins (or use specific domain)
           },
           {
             key: "Access-Control-Allow-Methods",
@@ -18,10 +18,6 @@ const nextConfig: NextConfig = {
           {
             key: "Access-Control-Allow-Headers",
             value: "Content-Type, Authorization",
-          },
-          {
-            key: "Access-Control-Allow-Credentials",
-            value: "true",
           },
         ],
       },
